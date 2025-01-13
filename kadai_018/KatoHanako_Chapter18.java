77813e72e7707f2140f2e140cb8a944ec019104f
yuichi0720 <sano1620@gmail.com>
2025-01-13 23:08:10 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+
+	 // 名前をセットするメソッド
+    public void setGivenName() {
+        this.givenName = "花子";
+    }
+   
+    // 固有の紹介内容
+   public void eachIntroduce() {
+        System.out.println("趣味は読書です");
+    }
+}
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+
+	 // 名前をセットするメソッド
+    public void setGivenName() {
+       this.givenName = "一郎";
+    
+    }
+    
+    // 固有の紹介内容
+  public void eachIntroduce() {
+        System.out.println("好きな食べ物はリンゴです");
+    }
+}
